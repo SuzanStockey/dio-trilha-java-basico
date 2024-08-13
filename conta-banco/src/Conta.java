4dbec58b214812619f2d0404103bea638a158e1f
Suzan Stockey Pereira <50267552+SuzanStockey@users.noreply.github.com>
2024-08-13 20:37:48 -0300

    Atualizar o Conta.java
    Adição do método toString().

--- conta-banco/src/Conta.java
+++ conta-banco/src/Conta.java
@@ -39,4 +39,17 @@ public class Conta {
     public void setSaldo(Boolean saldo) {
         this.saldo = saldo;
     }
+
+    public String toString(){
+    StringBuilder sb = new StringBuilder();
+    sb.append("Olá ");
+    sb.append(nomeCliente);
+    sb.append(", obrigado por criar uma conta em nosso banco, sua agência é ");
+    sb.append(agencia);
+    sb.append(", conta ");
+    sb.append(numero);
+    sb.append(" e seu saldo ");
+    sb.append(conta.getSaldo());
+    sb.append(" já está disponível para saque");
+    return sb.toString();
 }
