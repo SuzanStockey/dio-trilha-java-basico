cfc82fb9a3f59992d22976880c6caf30d9f47526
Suzan Stockey Pereira <98424651+suzan-isystem@users.noreply.github.com>
2024-08-15 16:27:23 -0300

    Tratamento de erro para caso o usuário insira dados de número de conta e saldo que não sejam números. Adição de comentários para o javadoc.

--- conta-banco/src/Conta.java
+++ conta-banco/src/Conta.java
@@ -1,55 +1,123 @@
+import java.text.NumberFormat;
+
+/**
+ * Representa uma conta bancária.
+ *
+ * Esta classe armazena informações sobre uma conta bancária, incluindo o número da conta,
+ * a agência, o nome do cliente e o saldo. Também fornece métodos para acessar e atualizar
+ * essas informações, além de gerar uma representação em string da conta.
+ *
+ * <p>Os atributos são acessíveis e modificáveis através dos métodos getters e setters.</p>
+ *
+ * @author Suzan Stockey Pereira
+ * @version 20240815
+ */
+
 public class Conta {
 
     private Integer numero;
     private String agencia;
     private String nomeCliente;
-    private Boolean saldo;
+    private Double saldo;
 
+    /**
+     * Cria uma nova instância de {@code Conta} com valores padrão.
+     */
     public Conta() {
     }
 
+    /**
+     * Retorna o número da conta.
+     *
+     * @return o número da conta
+     */
     public Integer getNumero() {
         return numero;
     }
 
+    /**
+     * Define o número da conta.
+     *
+     * @param numero o novo número da conta
+     */
     public void setNumero(Integer numero) {
         this.numero = numero;
     }
 
+    /**
+     * Retorna a agência da conta.
+     *
+     * @return a agência da conta
+     */
     public String getAgencia() {
         return agencia;
     }
 
+    /**
+     * Define a agência da conta.
+     *
+     * @param agencia a nova agência da conta
+     */
     public void setAgencia(String agencia) {
         this.agencia = agencia;
     }
 
+    /**
+     * Retorna o nome do cliente associado à conta.
+     *
+     * @return o nome do cliente
+     */
     public String getNomeCliente() {
         return nomeCliente;
     }
 
+    /**
+     * Define o nome do cliente associado à conta.
+     *
+     * @param nomeCliente o novo nome do cliente
+     */
     public void setNomeCliente(String nomeCliente) {
         this.nomeCliente = nomeCliente;
     }
 
-    public Boolean getSaldo() {
+    /**
+     * Retorna o saldo da conta.
+     *
+     * @return o saldo da conta
+     */
+    public Double getSaldo() {
         return saldo;
     }
 
-    public void setSaldo(Boolean saldo) {
+    /**
+     * Define o saldo da conta.
+     *
+     * @param saldo o novo saldo da conta
+     */
+    public void setSaldo(Double saldo) {
         this.saldo = saldo;
     }
 
-    public String toString(){
-    StringBuilder sb = new StringBuilder();
-    sb.append("Olá ");
-    sb.append(nomeCliente);
-    sb.append(", obrigado por criar uma conta em nosso banco, sua agência é ");
-    sb.append(agencia);
-    sb.append(", conta ");
-    sb.append(numero);
-    sb.append(" e seu saldo ");
-    sb.append(conta.getSaldo());
-    sb.append(" já está disponível para saque");
-    return sb.toString();
+    /**
+     * Retorna uma representação em string das informações da conta.
+     *
+     * A representação inclui uma mensagem personalizada para o cliente, mostrando
+     * o nome do cliente, a agência, o número da conta e o saldo formatado como moeda.
+     *
+     * @return uma string contendo as informações da conta formatadas
+     */
+    public String toString() {
+        String saldo = NumberFormat.getCurrencyInstance().format(this.saldo);
+        StringBuilder sb = new StringBuilder();
+        sb.append("Olá ");
+        sb.append(nomeCliente);
+        sb.append(", obrigado por criar uma conta em nosso banco, sua agência é ");
+        sb.append(agencia);
+        sb.append(", conta ");
+        sb.append(numero);
+        sb.append(" e seu saldo ");
+        sb.append(saldo);
+        sb.append(" já está disponível para saque");
+        return sb.toString();
+    }
 }
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,31 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * Classe que representa o terminal para interação com a conta bancária.
+ *
+ * Esta classe contém o método {@code main}, que serve como ponto de entrada para a aplicação.
+ * Ela permite que o usuário insira informações sobre uma conta bancária, como o número da agência,
+ * número da conta, nome do cliente e saldo. Em seguida, exibe essas informações formatadas para o usuário.
+ *
+ * <p>O programa utiliza o console para obter entradas do usuário e trata erros de entrada para garantir
+ * que o usuário insira valores válidos.</p>
+ *
+ * @author Suzan Stockey Pereira
+ * @version 20240815
+ */
+
 public class ContaTerminal {
+
+    /**
+     * O ponto de entrada da aplicação.
+     *
+     * Este método é o ponto de entrada principal para a aplicação Java. Ele inicia a execução
+     * do programa e solicita ao usuário que insira informações sobre a conta bancária. Após coletar
+     * todas as informações, o método exibe uma representação formatada da conta usando o método {@code toString}.
+     *
+     * @param args um array de {@code String} que contém os argumentos passados na linha de comando
+     */
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -10,15 +35,37 @@ public class ContaTerminal {
         conta.setAgencia(scanner.nextLine());
 
         System.out.println("Por favor, digite o número da Conta!");
-        conta.setNumero(scanner.nextInt());
-        scanner.nextLine();
+        boolean sucesso;
+        do {
+            sucesso = true;
+            try {
+                conta.setNumero(scanner.nextInt());
+                scanner.nextLine();
+
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                sucesso = false;
+                System.out.println("Valor incorreto para Número da Conta. Por favor, digite novamente.");
+            }
+        } while (!sucesso);
 
         System.out.println("Por favor, digite o Nome do Cliente!");
         conta.setNomeCliente(scanner.nextLine());
 
         System.out.println("Por favor, digite o Saldo da Conta!");
-        conta.setSaldo(scanner.nextDouble());
-        scanner.nextLine();
+        do {
+            sucesso = true;
+            try {
+                conta.setSaldo(scanner.nextDouble());
+                scanner.nextLine();
+
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                sucesso = false;
+                System.out.println("Valor incorreto para Saldo da Conta. Por favor, digite novamente.");
+            }
+        } while (!sucesso);
+
 
         System.out.println(conta.toString());
 
